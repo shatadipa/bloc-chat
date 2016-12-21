b171088f99ff0fc7ee9100fd147d67a3bce23dcc
Shatadipa Chakraborty <shatadipa1986@gmail.com>
2016-12-20 22:55:49 -0800

    Chapter 2 Checkpoint

---
+++ dist/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,10 @@
+(function () {
+
+    function HomeCtrl($scope, Room) {
+        $scope.rooms = Room.all;
+    }
+
+    angular
+        .module('blocChat')
+        .controller('HomeCtrl', ['$scope', 'Room', HomeCtrl]);
+})();
